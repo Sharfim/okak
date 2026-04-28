82adeb20824cba166d05980a0e6a2c131bd9643d
Sharfim <Sharfim@yandex.ru>
2026-04-28 14:35:13 +0600

    import bot

--- handler/import_bot.py
+++ handler/import_bot.py
@@ -1,10 +1,12 @@
+from telebot import Telebot
 import os
 import sys
 
-def bot_import():
+def bot_import() -> Telebot:
     current_dir = os.path.dirname(__file__)
     path_bot = os.path.abspath(os.path.join(current_dir, ".."))
 
     sys.path.append(path_bot)
 
     from bot import bot
+    return bot
